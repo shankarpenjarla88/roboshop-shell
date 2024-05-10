198716e2d774af65c1efef8edc8542e447e9ae33
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-09 21:32:11 -0700

    Mysqldb

---
+++ mysql.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+date=$(date +%F)
+scriptname=$0
+logfile=/tmp/$scriptname-$date.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+validate() {
+    if [ $1 -ne 0 ]
+    then 
+      echo -e "$2 IS  $R FAILED $N"
+    else
+      echo -e "$2 IS $G SUCCESS $N"
+    fi
+}
+
+userid=$( id -u )
+
+if [ $userid -ne 0 ]
+then 
+  echo "ERROR:PLEASE LOGIN TO ADMIN ACCESS"
+  exit 1
+else
+  echo "NOW YOU ARE IN THE ROOT ACCESS"
+fi
+
+yum module disable mysql -y 
+
+cp /root/roboshop-shell/mysql.repo  /etc/yum.repos.d/mysql.repo
+
+yum install mysql-community-server -y
+
+systemctl enable mysqld
+
+systemctl start mysqld
+
