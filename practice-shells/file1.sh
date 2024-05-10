f757404981618c47b12f4d5bbb0479fc0d37e520
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-10 03:17:30 -0700

    Files1

---
+++ practice-shells/file1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash ---> it is sheband to execute linux we are writing.
+
+l1=vivo
+l2=oppo
+
+echo "$l1 have excellent features, it is better and best for middle class people"
+echo " Even $l2 have diffrent features and compare with oopo"
+
