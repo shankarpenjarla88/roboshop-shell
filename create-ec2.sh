32932676571f3379f5758864091fd4168967e3ba
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-10 22:28:59 -0700

    awscommandline

--- create-ec2.sh
+++ create-ec2.sh
@@ -7,12 +7,7 @@ Security_grp=sg-0c9760a3839e63c1dcho
 
 for i in "${Names[@]}"
 do
-  if [ [$i == "mongodb" || $i == "mysql" ] ];
-  then
-    Instance_Type="t2.micro"
-  else
-    Instance_Type="t2.micro"
-  fi
+  
   echo "Creatio of Instances:$i"
   aws ec2 run-instances --image-id $Image_id --count 1 --instance-type $Instance_Type  --security-group-ids $Security_grp 
   
