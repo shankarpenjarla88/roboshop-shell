0dfc74f241716cb6724350f0c9b363e0524a53be
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-11 00:49:13 -0700

    AWSEC2INSTANCES

--- create-ec2.sh
+++ create-ec2.sh
@@ -8,12 +8,12 @@ Security_grp=sg-0c9760a3839e63c1dcho
 
 for i in "${Names[@]}"
 do
-   if [[ $i == "mongodb" || $i == "mysql"]]
-   then
+    if [[ $i == "mongodb" || $i == "mysql"]]
+    then
        $Instance_type = "t2.micro" 
-   else
+    else
       $Instance_type = "t2.micro"
-   fi
+    fi 
     echo "Creatio of Instances:$i"
     aws ec2 run-instances --image-id $Image_id --count 1  --instance-type $Instance_type  --security-group-ids $Security_grp  --tag-specifications "ResourceType=instance,Tags=[{Key=$Names,Value=$i}]"
 done 
