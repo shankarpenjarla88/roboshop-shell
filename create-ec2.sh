05a0e56c3b7703227fd0f9d0dc607ec6d3052abc
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-11 00:22:31 -0700

    AWSInstances

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 Names=("mongodb" "catalogue" "cart" "mysql" "redis" "rabbitmq" "user" "shipping" "payment" "dispatch" "web")
+Instance_type=" "
+Image_id=ami-0f3c7d07486cad139 
+Security_grp=sg-0c9760a3839e63c1dcho  
+
 
 
 for i in "${Names[@]}"
 do
-
-  echo "Creatio of Instances:$i"
-  aws ec2 run-instances --image-id ami-0f3c7d07486cad139   --count 1 --instance-type t2.micro --security-group-ids sg-0c9760a3839e63c1dcho 
+    if [ [ $i == "mangodb" || $i == "mysql"]]
+    then
+      Instance_type = "t3.micro"
+    else
+      Instance_type = "t2.micro"
+    fi
+    echo "Creatio of Instances:$i"
+    aws ec2 run-instances \--image-id $Image_id \--count 1 \ --instance-type $Instance_type \ --security-group-ids $Security_grp \ --tag-specifications "ResourceType=instance,Tags=[{Key=$Names,Value=$i}]"
 done 
