5850818b87d92846c1e6844a06b30a51eee4fd82
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-10 22:35:01 -0700

    aws

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
 Names=("mongodb" "catalogue" "cart" "mysql" "redis" "rabbitmq" "user" "shipping" "payment" "dispatch" "web")
-Image_id="ami-0f3c7d07486cad139" 
-Instance_Type="t2.micro"
-Security_grp="sg-0c9760a3839e63c1dcho"
+
 
 for i in "${Names[@]}"
 do
-  
-  echo "Creatio of Instances:$i"
 
-  aws ec2 run-instances --image-id $Image_id --count 1 --instance-type $Instance_Type --security-group-ids $Security_grp 
-  
+  echo "Creatio of Instances:$i"
+  aws ec2 run-instances --image-id ami-0f3c7d07486cad139   --count 1 --instance-type t2.micro --security-group-ids sg-0c9760a3839e63c1dcho 
 done 
