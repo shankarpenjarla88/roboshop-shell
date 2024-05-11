b2280ec77cff6bd27baf19dc4097a68c885c0584
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-11 00:52:45 -0700

    instancesincommandline

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,7 +2,8 @@
 
 Names=("mongodb" "catalogue" "cart" "mysql" "redis" "rabbitmq" "user" "shipping" "payment" "dispatch" "web")
 Image_id=ami-0f3c7d07486cad139 
-Security_grp=sg-0c9760a3839e63c1dcho  
+Security_grp=sg-0c9760a3839e63c1dcho 
+Instance_type=""
 
 
 
@@ -10,9 +11,9 @@ for i in "${Names[@]}"
 do
     if [ [ $i == "mongodb" || $i == "mysql"] ]
     then
-       $Instance_type = "t2.micro" 
+      Instance_type= "t2.micro" 
     else
-      $Instance_type = "t2.micro"
+      Instance_type= "t2.micro"
      fi 
     echo "Creatio of Instances:$i"
     aws ec2 run-instances --image-id $Image_id --count 1  --instance-type $Instance_type  --security-group-ids $Security_grp  --tag-specifications "ResourceType=instance,Tags=[{Key=$Names,Value=$i}]"
