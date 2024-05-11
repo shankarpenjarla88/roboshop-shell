ab90f01d2806b7bdd3be5640064450506d36bcdd
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-10 22:32:25 -0700

    AWSCOMMANDLINE

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 Names=("mongodb" "catalogue" "cart" "mysql" "redis" "rabbitmq" "user" "shipping" "payment" "dispatch" "web")
-Image_id=ami-0f3c7d07486cad139 
+Image_id="ami-0f3c7d07486cad139" 
 Instance_Type="t2.micro"
-Security_grp=sg-0c9760a3839e63c1dcho 
+Security_grp="sg-0c9760a3839e63c1dcho"
 
 for i in "${Names[@]}"
 do
