5fd64b10fcdb93405b5ed011f2b51c520b18697d
shankarpenjarla88 <shankarpenjarla88@gmail.com>
2024-05-11 00:41:46 -0700

    InstancesMultis

--- create-ec2.sh
+++ create-ec2.sh
@@ -11,9 +11,9 @@ for i in "${Names[@]}"
 do
    if [[ $i == "mangodb" || $i == "mysql"]]
    then
-     Instance_type = "t2.micro"
+       $Instance_type = "t2.micro" "
    else
-     Instance_type = "t2.micro"
+      $Instance_type = "t2.micro"
    fi
     echo "Creatio of Instances:$i"
     aws ec2 run-instances --image-id $Image_id --count 1  --instance-type $Instance_type  --security-group-ids $Security_grp  --tag-specifications "ResourceType=instance,Tags=[{Key=$Names,Value=$i}]"
